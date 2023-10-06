7c7f0eaf8f7e3dbc99976467f8d909afdad26360
Oragif <Mikkelhelsing1@gmail.com>
2023-10-06 13:39:40 +0200

    Using more efficient Annotated class finder

--- src/main/java/com/oragif/jxpress/routing/Route.java
+++ src/main/java/com/oragif/jxpress/routing/Route.java
@@ -1,11 +1,13 @@
 package com.oragif.jxpress.routing;
 
 import com.oragif.jxpress.worker.Method;
+import org.atteo.classindex.IndexAnnotated;
 
 import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@IndexAnnotated
 public @interface Route {
     String path() default "/";
     String provides() default "text/plain";
